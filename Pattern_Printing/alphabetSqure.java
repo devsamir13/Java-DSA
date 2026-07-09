93590dfa1be826db251196309b21206f02dda493
devsamir13 <samir5827245@gmail.com>
2026-07-10 00:39:02 +0530

    learning

---
+++ Pattern_Printing/alphabetSqure.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class alphabetSqure {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print((char)(i+64)+ " ");
+        //     }
+        //     System.out.println();
+        // }
+
+         for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print((char)(i+96)+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
