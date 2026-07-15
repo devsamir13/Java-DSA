014f435de59fecf92e6c24258a3f902c588c79e9
devsamir13 <samir5827245@gmail.com>
2026-07-15 23:28:05 +0530

    learning

---
+++ Pattern_Printing/alphabetTriangle2.java
@@ -0,0 +1,31 @@
+//   1
+//   A B
+//   1 2 3
+//   A B C D
+//   1 2 3 4 5
+//   A B C D E F
+
+import java.util.Scanner;
+
+public class alphabetTriangle2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+            } else {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print((char) (j + 64) + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
